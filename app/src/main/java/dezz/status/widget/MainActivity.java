90e29a8fa4a00946e4f4d25c024e3bcb3d0393af
DezzK <18627923+DezzK@users.noreply.github.com>
2025-11-19 01:07:22 +0500

    refactor: extract view binding logic into separate class

--- app/src/main/java/dezz/status/widget/MainActivity.java
+++ app/src/main/java/dezz/status/widget/MainActivity.java
@@ -22,14 +22,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.text.method.LinkMovementMethod;
-import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.app.ActivityCompat;
 
 import java.util.List;
@@ -39,10 +37,6 @@ import dezz.status.widget.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     public static final int PERMISSION_REQUEST_CODE = 1001;
     public static final int OVERLAY_PERMISSION_REQUEST_CODE = 1002;
-
-    private interface ValueTextFormatter {
-        String formatValueText(int progress);
-    }
 
     private Preferences prefs;
 
@@ -89,21 +83,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        bindCheckbox(binding.useColorIconsSwitch, prefs.useColorIcons);
-        bindCheckbox(binding.showDateSwitch, prefs.showDate);
-        bindCheckbox(binding.showTimeSwitch, prefs.showTime);
-        bindCheckbox(binding.showDaySwitch, prefs.showDayOfTheWeek);
-        bindCheckbox(binding.showWiFiSwitch, prefs.showWifiIcon);
-        bindCheckbox(binding.showGnssSwitch, prefs.showGnssIcon);
-        bindCheckbox(binding.showFullDayAndMonthSwitch, prefs.showFullDayAndMonth);
-        bindCheckbox(binding.oneLineLayoutSwitch, prefs.oneLineLayout);
+        ViewBinder binder = new ViewBinder(this);
 
-        bindSizeSeekbar(binding.iconSizeSeekBar, binding.iconSizeValueText, prefs.iconSize);
-        bindSizeSeekbar(binding.timeFontSizeSeekBar, binding.timeFontSizeValueText, prefs.timeFontSize);
-        bindSizeSeekbar(binding.dateFontSizeSeekBar, binding.dateFontSizeValueText, prefs.dateFontSize);
-        bindSizeSeekbar(binding.spacingBetweenTextsAndIconsSeekBar, binding.spacingBetweenTextsAndIconsValueText, prefs.spacingBetweenTextsAndIcons);
-        bindOffsetSeekbar(binding.adjustTimeYSeekBar, binding.adjustTimeYValueText, prefs.adjustTimeY);
-        bindOffsetSeekbar(binding.adjustDateYSeekBar, binding.adjustDateYValueText, prefs.adjustDateY);
+        binder.bindCheckbox(binding.useColorIconsSwitch, prefs.useColorIcons);
+        binder.bindCheckbox(binding.showDateSwitch, prefs.showDate);
+        binder.bindCheckbox(binding.showTimeSwitch, prefs.showTime);
+        binder.bindCheckbox(binding.showDaySwitch, prefs.showDayOfTheWeek);
+        binder.bindCheckbox(binding.showWiFiSwitch, prefs.showWifiIcon);
+        binder.bindCheckbox(binding.showGnssSwitch, prefs.showGnssIcon);
+        binder.bindCheckbox(binding.showFullDayAndMonthSwitch, prefs.showFullDayAndMonth);
+        binder.bindCheckbox(binding.oneLineLayoutSwitch, prefs.oneLineLayout);
+
+        binder.bindSizeSeekbar(binding.iconSizeSeekBar, binding.iconSizeValueText, prefs.iconSize);
+        binder.bindSizeSeekbar(binding.timeFontSizeSeekBar, binding.timeFontSizeValueText, prefs.timeFontSize);
+        binder.bindSizeSeekbar(binding.dateFontSizeSeekBar, binding.dateFontSizeValueText, prefs.dateFontSize);
+        binder.bindSizeSeekbar(binding.spacingBetweenTextsAndIconsSeekBar, binding.spacingBetweenTextsAndIconsValueText, prefs.spacingBetweenTextsAndIcons);
+        binder.bindOffsetSeekbar(binding.adjustTimeYSeekBar, binding.adjustTimeYValueText, prefs.adjustTimeY);
+        binder.bindOffsetSeekbar(binding.adjustDateYSeekBar, binding.adjustDateYValueText, prefs.adjustDateY);
     }
 
     private void startWidgetService() {
@@ -114,46 +110,6 @@ public class MainActivity extends AppCompatActivity {
     private void stopWidgetService() {
         prefs.widgetEnabled.set(false);
         stopService(new Intent(this, WidgetService.class));
-    }
-
-    private void bindCheckbox(SwitchCompat checkbox, Preferences.Bool preference) {
-        checkbox.setChecked(preference.get());
-        checkbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            preference.set(isChecked);
-            if (WidgetService.isRunning()) {
-                WidgetService.getInstance().applyPreferences();
-            }
-        });
-    }
-
-    private void bindSizeSeekbar(SeekBar seekBar, TextView valueText, Preferences.Int preference) {
-        bindSeekbar(seekBar, valueText, preference, value -> String.format(getString(R.string.size_value_format), value));
-    }
-
-    private void bindOffsetSeekbar(SeekBar seekBar, TextView valueText, Preferences.Int preference) {
-        bindSeekbar(seekBar, valueText, preference, value -> String.format((value > 0 ? "+" : "") + getString(R.string.size_value_format), value));
-    }
-
-    private void bindSeekbar(SeekBar seekBar, TextView valueText, Preferences.Int preference, ValueTextFormatter formatter) {
-        int progress = preference.get();
-        seekBar.setProgress(progress);
-        valueText.setText(formatter.formatValueText(progress));
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                preference.set(progress);
-                valueText.setText(formatter.formatValueText(progress));
-                if (WidgetService.isRunning()) {
-                    WidgetService.getInstance().applyPreferences();
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
     }
 
     private void requestPermissions() {
---
+++ app/src/main/java/dezz/status/widget/ViewBinder.java
@@ -0,0 +1,59 @@
+package dezz.status.widget;
+
+import android.content.Context;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.SwitchCompat;
+
+public final class ViewBinder {
+    private final Context context;
+
+    public ViewBinder(Context context) {
+        this.context = context;
+    }
+
+    public void bindCheckbox(SwitchCompat checkbox, Preferences.Bool preference) {
+        checkbox.setChecked(preference.get());
+        checkbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            preference.set(isChecked);
+            if (WidgetService.isRunning()) {
+                WidgetService.getInstance().applyPreferences();
+            }
+        });
+    }
+
+    public void bindSizeSeekbar(SeekBar seekBar, TextView valueText, Preferences.Int preference) {
+        bindSeekbar(seekBar, valueText, preference, value -> String.format(context.getString(R.string.size_value_format), value));
+    }
+
+    public void bindOffsetSeekbar(SeekBar seekBar, TextView valueText, Preferences.Int preference) {
+        bindSeekbar(seekBar, valueText, preference, value -> String.format((value > 0 ? "+" : "") + context.getString(R.string.size_value_format), value));
+    }
+
+    public void bindSeekbar(SeekBar seekBar, TextView valueText, Preferences.Int preference, ValueTextFormatter formatter) {
+        int progress = preference.get();
+        seekBar.setProgress(progress);
+        valueText.setText(formatter.formatValueText(progress));
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                preference.set(progress);
+                valueText.setText(formatter.formatValueText(progress));
+                if (WidgetService.isRunning()) {
+                    WidgetService.getInstance().applyPreferences();
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    public interface ValueTextFormatter {
+        String formatValueText(int progress);
+    }
+}
